8e318ccbe1d4fb0d3740b677d025f58b521530d4
Your Name <you@example.com>
2026-04-02 20:51:25 +0530

    fix: make STATIC_URL absolute to resolve CSS 404s

--- core/settings.py
+++ core/settings.py
@@ -130,7 +130,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
@@ -143,8 +143,7 @@ STORAGES = {
     },
 
 }
-
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
